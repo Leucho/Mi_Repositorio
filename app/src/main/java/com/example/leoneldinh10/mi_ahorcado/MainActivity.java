bccac39e25aa2b4346bda1886649775b79df91a1
FedericoMarinangeli <fedemarinangeli@gmail.com>
2017-04-21 11:12:20 -0300

    jhajajahjfhf

--- app/src/main/java/com/example/leoneldinh10/mi_ahorcado/MainActivity.java
+++ app/src/main/java/com/example/leoneldinh10/mi_ahorcado/MainActivity.java
@@ -13,7 +13,7 @@ import static java.lang.Thread.sleep;
 
 public class MainActivity extends AppCompatActivity {
 
-    String palabra = "fedeprueba";
+    String palabra = "leo";
     int total_coincidencias_varGlobal=0;
 
     @Override
